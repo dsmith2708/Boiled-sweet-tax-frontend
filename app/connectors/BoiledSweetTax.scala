4d876251ee51bad1b1dbb61c5bf22a71f1fda86d
stan501 <stanton.a.ball@capgemini.com>
2025-09-15 11:52:59 +0100

    gg

--- app/connectors/BoiledSweetTax.scala
+++ app/connectors/BoiledSweetTax.scala
@@ -11,10 +11,12 @@ class BoiledSweetTax @Inject()(ws: WSClient)(implicit ec: ExecutionContext) {
   private val url = "http://localhost:9002/boiled-sweet-tax-registration/submit"
 
   def getUtrValue: Future[String] = {
-    val responseValue = ws.url(url).post("").map{
+    val responseValue = ws.url(url).post("").map {
       response => {
         (response.json \ "utr").as[String]
       }
     }
+    println("gg")
     responseValue
   }
+}
